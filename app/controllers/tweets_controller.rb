da279f2cba33c5087e9cd2e4bc21feb3a19f34d6
ismailaidar <ismailaidar@gmail.com>
2020-07-21 10:43:32 -0400

    protect tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
 
   # GET /tweets
   # GET /tweets.json
